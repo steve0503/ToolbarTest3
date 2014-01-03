8b866f45919fe7477c56fbc9801937ae43615b56
steve0503 <leehr76@gmail.com>
2014-01-03 14:26:59 +0900

    Initial Commit

---
+++ ToolbarTest3Tests/ToolbarTest3Tests.m
@@ -0,0 +1,34 @@
+//
+//  ToolbarTest3Tests.m
+//  ToolbarTest3Tests
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ToolbarTest3Tests : XCTestCase
+
+@end
+
+@implementation ToolbarTest3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
