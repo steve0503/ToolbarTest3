0399b723b7334a4770a632986874bb00f98c7f08
steve0503 <leehr76@gmail.com>
2014-01-03 14:59:16 +0900

    ToolbarTest 완료했습니다.
    완료.

--- ToolbarTest3/ViewController.m
+++ ToolbarTest3/ViewController.m
@@ -14,10 +14,68 @@
 
 @implementation ViewController
 
+
+-(IBAction)handleBarButtonClick:(id)sender{
+    
+    UIBarButtonItem *button = (UIBarButtonItem*)sender;
+    
+    if (1 == button.tag) {
+        NSLog(@"왼쪽버튼");
+    }
+    else
+    {
+        NSLog(@"오른쪽버튼");
+    
+    }
+    
+}
+
+-(void)handleRefresh:(id)sender{
+    
+    NSLog(@"Refresh!");
+    
+}
+
+-(void)handleButton:(id)sender{
+    
+    NSLog(@"Button Clicked!");
+    
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectMake(0, 160, 320, 60)];
+    
+    UIBarButtonItem *titleButton = [[UIBarButtonItem alloc] initWithTitle:@"Button" style:UIBarButtonItemStyleDone target:self action:@selector(handleButton:)];
+    
+    
+    
+    NSArray *segments = [NSArray arrayWithObjects:@"A",@"B",@"C", nil];
+    
+    UISegmentedControl *segment = [[UISegmentedControl alloc] initWithItems:segments];
+    
+    segment.frame = CGRectMake(0, 0, 140, 44);
+    
+    UIBarButtonItem *segmentButton = [[UIBarButtonItem alloc] initWithCustomView:segment];
+    
+    UIBarButtonItem *space = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFlexibleSpace target:nil action:nil];
+    
+    UIBarButtonItem *refreshButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRefresh target:self action:@selector(handleRefresh:)];
+    
+    NSArray *items = [NSArray arrayWithObjects:titleButton,segmentButton,space,refreshButton, nil];
+    
+    [toolbar setItems:items];
+    
+    [self.view addSubview:toolbar];
+    
+    
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
